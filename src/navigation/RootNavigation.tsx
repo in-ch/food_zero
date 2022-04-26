16ba4b71101afc42bf90a4a155c97098d59b9bff
in-ch <sxin2949@naver.com>
2022-04-26 23:59:48 +0900

    Home setting

---
+++ src/navigation/RootNavigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../c_screens/Home/Home';
+
+export type RootTabParamList = {
+  //   Home: {
+  //   };
+};
+
+const RootNavigation = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigation;
