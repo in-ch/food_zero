314ff9c1ba6591ea3aedf61a9dd1a0f4c8ce763f
in-ch <sxin2949@naver.com>
2022-05-09 22:16:32 +0900

    feat: 식품추가하기 페이지 생성 완료

--- src/c_components/DatePicker.tsx
+++ src/c_components/DatePicker.tsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 import DatePicker from 'react-native-date-picker';
+import styled from 'styled-components/native';
 import moment from 'moment';
 import TextBox from './TextBox';
+import {cWidth} from '~/utills/constants';
 
+const Container = styled.View`
+  min-width: ${cWidth * 0.75};
+`;
 const DDatePicker = () => {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
   return (
-    <>
+    <Container>
       <TouchableWithoutFeedback onPress={() => setOpen(true)}>
         <TextBox text={String(moment(date).format('YYYY / MM / DD'))} />
       </TouchableWithoutFeedback>
@@ -27,7 +32,7 @@ const DDatePicker = () => {
           setOpen(false);
         }}
       />
-    </>
+    </Container>
   );
 };
 
--- src/c_components/Header/Header.tsx
+++ src/c_components/Header/Header.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import styled from 'styled-components/native';
+import {Image} from 'react-native';
 import {cWidth, nomalizes, statusBarHeight} from '@utills/constants';
 import {SizedBox} from '@components/SizedBox';
 import {cssUtil} from '@utills/cssUtil';
 import Images from '@assets/images';
-import {Image} from 'react-native';
 
 const Container = styled.View`
   background-color: #fff;
 `;
 const Wrapper = styled.View`
   width: 100%;
+  max-width: 100%;
   height: ${nomalizes[35]}px;
   display: flex;
   flex-direction: row;
@@ -24,22 +25,38 @@ const IconContatiner = styled.TouchableOpacity`
   flex-direction: row;
   ${cssUtil.doubleCenter}
 `;
-const TextContainer = styled.View`
-  width: ${cWidth - nomalizes[30]};
+const TextContainer = styled.View<TextContainerProps>`
+  width: ${props =>
+    props.button ? cWidth - nomalizes[80] : cWidth - nomalizes[30]};
 `;
-const TText = styled.Text`
+const TText = styled.Text<TextContainerProps>`
   position: relative;
-  left: -${nomalizes[15]}px;
+  left: ${props => (props.button ? nomalizes[15] : -nomalizes[15])};
   font-size: ${nomalizes[14]}px;
   font-weight: bold;
 `;
-
+const Button = styled.TouchableOpacity`
+  width: ${nomalizes[45]}px;
+  height: ${nomalizes[25]}px;
+  margin-right: ${nomalizes[10]}px;
+  border-radius: ${nomalizes[7]}px;
+  display: flex;
+  background-color: #ff6c63;
+  ${cssUtil.doubleCenter};
+`;
+const ButtonText = styled.Text`
+  color: white;
+`;
+interface TextContainerProps {
+  button?: boolean;
+}
 interface HeaderProps {
   text: string;
   back: () => void;
+  button?: () => void;
 }
 
-const Header = ({text, back}: HeaderProps) => {
+const Header = ({text, back, button}: HeaderProps) => {
   return (
     <Container>
       <SizedBox.Custom margin={statusBarHeight} />
@@ -54,9 +71,16 @@ const Header = ({text, back}: HeaderProps) => {
             source={Images.back}
           />
         </IconContatiner>
-        <TextContainer>
-          <TText style={{textAlign: 'center'}}>{text}</TText>
+        <TextContainer button={button ? true : false}>
+          <TText button={button ? true : false} style={{textAlign: 'center'}}>
+            {text}
+          </TText>
         </TextContainer>
+        {button && (
+          <Button>
+            <ButtonText>완료</ButtonText>
+          </Button>
+        )}
       </Wrapper>
     </Container>
   );
